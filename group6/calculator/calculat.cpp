16598e03c0e17815e1c7838e1f65c171001c0d05
Betsi <meseretgete2212@gmail.com>
2025-04-08 09:18:45 +0300

    this is a simple calulation multiplication

---
+++ group6/calculator/calculat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1 = 7.5;   // First number
+    double num2 = 4.0;   // Second number
+
+    double result = num1 * num2;
+
+    cout << "Number 1: " << num1 << endl;
+    cout << "Number 2: " << num2 << endl;
+    cout << "Multiplication result: " << result << endl;
+
+    return 0;
+}
